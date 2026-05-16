665797ddd734413a9701aff2fc75f36eb6cd9d3a
omar ghonaim <omar.ghonaim@gradion.com>
2026-05-16 14:56:11 +0300

    Update asset path in Helpers.php to streamline storage URL generation by removing 'app/public' prefix.

--- app/CentralLogics/Helpers.php
+++ app/CentralLogics/Helpers.php
@@ -3285,7 +3285,7 @@ class Helpers
         }
 
         if ($data && Storage::disk('public')->exists($path . '/' . $data)) {
-            return asset('storage/app/public') . '/' . $path . '/' . $data;
+            return asset('storage') . '/' . $path . '/' . $data;    
         }
 
         if (request()->is('api/*')) {
